196335a8fa62a245c665a74ac557e6555d2af9c4
Cassandra Corzine <cassie@ties.org>
2019-01-27 22:13:30 -0500

    Basic Outline of Blackjack

---
+++ blackjack/blackjack.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to Blackjack")
+	fmt.Println("Make a Deck of Cards")
+	fmt.Println("Shuffle the Deck of Cards")
+	fmt.Println("Deal Two Cards To Player and Dealer,Make Sure the One of the Dealers Cards are Down")
+	fmt.Println("Dealer Ask Hit")
+	fmt.Println("If Hit. Deal Another Card, If Stand Don't")
+	fmt.Println("If Over 21 They Bust")
+	fmt.Println("Dealer Hit Till 17 or More")
+	fmt.Println("Say if Player Wins or Loses")
+	fmt.Println("End Game")
+}
