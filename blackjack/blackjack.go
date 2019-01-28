c5b718206004c6db9e2b00aa1ec657be5697e919
Cassandra Corzine <cassie@ties.org>
2019-01-27 22:36:18 -0500

    added mcdeck and souffle

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 )
 
+func mcdeck() {
+	fmt.Println("Make a Deck of Cards")
+}
+
+func souffle() {
+	fmt.Println("Shuffle the Deck of Cards")
+}
+
 func main() {
 	fmt.Println("Welcome to Blackjack")
-	fmt.Println("Make a Deck of Cards")
-	fmt.Println("Shuffle the Deck of Cards")
+	mcdeck()
+	souffle()
 	fmt.Println("Deal Two Cards To Player and Dealer,Make Sure the One of the Dealers Cards are Down")
 	fmt.Println("Dealer Ask Hit")
 	fmt.Println("If Hit. Deal Another Card, If Stand Don't")
