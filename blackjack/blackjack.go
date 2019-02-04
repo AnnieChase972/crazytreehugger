d8bc90f7584ffdc7394835cdaabf02de77a35d32
Cassandra Corzine <cassie@ties.org>
2019-02-03 21:17:33 -0500

    make a deck of cards and print them

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -4,23 +4,58 @@ import (
 	"fmt"
 )
 
-func mcdeck() {
-	fmt.Println("Make a Deck of Cards")
+func mcdeck() []string {
+	barney := []string{" of clubs", " of spades", " of hearts", " of diamonds"}
+	dank := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+	var holdywater []string
+	for _, chungha := range barney {
+		for _, bueller := range dank {
+			holdywater = append(holdywater, bueller + chungha)
+		}
+	}
+	return holdywater
+}
+
+func picture(florida []string) {
+	for _, loopy := range florida {
+		fmt.Println(loopy)
+	}
 }
 
 func souffle() {
 	fmt.Println("Shuffle the Deck of Cards")
 }
 
+func cardinal() {
+	fmt.Println("Deal Two Cards To Player and Dealer,Make Sure the One of the Dealers Cards are Down")
+}
+
+func ko() {
+	fmt.Println("Dealer Ask Hit")
+}
+
+func anoder1() {
+	fmt.Println("If Hit. Deal Another Card, If Stand Don't")
+}
+
+func bar() {
+	fmt.Println("If Over 21 They Bust")
+}
+
+func nct() {
+	fmt.Println("Dealer Hit Till 17 or More")
+}
+
 func main() {
 	fmt.Println("Welcome to Blackjack")
-	mcdeck()
+	florida := mcdeck()
+	picture(florida)
 	souffle()
-	fmt.Println("Deal Two Cards To Player and Dealer,Make Sure the One of the Dealers Cards are Down")
-	fmt.Println("Dealer Ask Hit")
-	fmt.Println("If Hit. Deal Another Card, If Stand Don't")
-	fmt.Println("If Over 21 They Bust")
-	fmt.Println("Dealer Hit Till 17 or More")
+	cardinal()
+	ko()
+	anoder1()
+	bar()
+	nct()
 	fmt.Println("Say if Player Wins or Loses")
 	fmt.Println("End Game")
 }
